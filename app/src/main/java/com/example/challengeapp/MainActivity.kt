500694347ff7107e20d9687008196361e6dfb15c
mand027 <mando27tuzo@gmail.com>
2020-03-31 22:09:22 -0600

    finito

--- app/src/main/java/com/example/challengeapp/MainActivity.kt
+++ app/src/main/java/com/example/challengeapp/MainActivity.kt
@@ -3,24 +3,18 @@ package com.example.challengeapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-import java.lang.Exception
-import java.math.BigInteger
-import java.security.MessageDigest
-import java.sql.Timestamp
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var noticiaAdapter: NoticiaRecyclerAdapter
-    var jNews: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setRecyclerView()
-        Log.i("Noticias API", getNoticiasApiURL())
         NoticiaVolley(getNoticiasApiURL(), this, noticiaAdapter).callNewslAPI()
     }
 
